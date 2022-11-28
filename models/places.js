6e536bbe9fbeb9db5e1fdf319b2029b0d2c82aa8
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-11-28 13:18:55 -0800

    option to add new places, need to fix

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/rest1.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/rest2.jpg'
+  }]
